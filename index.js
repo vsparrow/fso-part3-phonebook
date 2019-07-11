50b15e1818909283faca7498025a8636ddbcb359
vsparrow.com <khan@vsparrow.com>
2019-07-11 00:07:28 +0000

    3.1: Phonebook backend step1

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 const express = require('express')
 const app = express();
 
+const persons = [
+    { 
+      "name": "Arto Hellas", 
+      "number": "040-123456",
+      "id": 1
+    },
+    { 
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523",
+      "id": 2
+    },
+    { 
+      "name": "Dan Abramov", 
+      "number": "12-43-234345",
+      "id": 3
+    },
+    { 
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122",
+      "id": 4
+    },
+    { 
+      "name": "Maz Jabroni", 
+      "number": "39-22-6457522",
+      "id": 5
+    }
+  ]
+
+app.get('/api/persons', (req,res)=>{
+    res.json(persons)
+})
 
 app.get('/',(req,res)=>{
     res.send('<h1>Hello World</h1>')
