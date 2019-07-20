f2a2a7969ca890c7dbb85512e06af2c86c4b95a9
khan@vsparrow <khan@vsparrow.com>
2019-07-20 15:43:58 +0000

    uibuild

---
+++ build/precache-manifest.ca4a4f97879a07d7e7e8dd9ae0489687.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f1b128efc705d248ee78ecd71bc7abe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edb70b9760340e495c07",
+    "url": "/static/js/2.42c16a5d.chunk.js"
+  },
+  {
+    "revision": "1bc264b6d4cd5319fa97",
+    "url": "/static/js/main.4d70b2c8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
