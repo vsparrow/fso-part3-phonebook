c13b059a734c6e5eb6deceadc37cdb6e375f55fd
khan@vsparrow <khan@vsparrow.com>
2019-07-25 23:36:21 +0000

    Add models/person.js : connect to mongodb

---
+++ models/person.js
@@ -0,0 +1,38 @@
+require('dotenv').config()  //remove after testing
+const mongoose = require('mongoose')
+const url = process.env.MONGODB_URI
+// console.log('url is ',url)
+console.log("connecting to URI: ", url)
+
+mongoose.connect(url, {useNewUrlParser: true})
+.then(result => console.log('connected to MongoDB'))
+.catch(error => console.log('error connecting to MongoDB:', error.message))
+
+const personSchema = new mongoose.Schema({
+	name: String,
+	number: String
+})
+
+personSchema.set('toJSON',{
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id
+		delete returnedObject.__v
+	}
+})
+
+//above seems to work
+// testing below
+
+// const Person = mongoose.model('Person', personSchema)
+
+// 	Person.find({}).then(res => {
+// 		console.log("phonebook:")
+// 		res.forEach(person => {
+// 			console.log(`${person.name} ${person.number}`)
+// 		})
+// 		mongoose.connection.close()	
+// 	})
+
+
+module.exports = mongoose.model('person', personSchema)
