81e4966d14f9229a8110fd3599240579b3d4b2a9
khan@vsparrow <khan@vsparrow.com>
2019-08-03 23:59:54 +0000

    MODEL persons : add minimum length validators

--- models/person.js
+++ models/person.js
@@ -12,8 +12,8 @@ mongoose.connect(url, {useNewUrlParser: true})
 .catch(error => console.log('error connecting to MongoDB:', error.message))
 
 const personSchema = new mongoose.Schema({
-	name: {type: String, required: true, unique: true},
-	number: {type: String, required: true}
+	name: {type: String, required: true, unique: true, minlength: 3},
+	number: {type: String, required: true, minlength: 8}
 })
 personSchema.plugin(uniqueValidator)
 
